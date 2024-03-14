db87dc6cb23fbeefbc0d8aa6e815c93fbc8ae83b
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-03-14 10:49:03 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App 타이틀',
+        // App의 전반적인 테마를 설정한다.
+        theme: ThemeData(
+          // 텍스트의 style을 미리 설정하고 호출해 사용 가능하다.
+          textTheme: TextTheme(
+            bodyText1: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar'),
+            backgroundColor: Colors.blue,
+          ),
+          body: Text('Body 입니다.'),
+          // BottomNavigationBar
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'school',
+              ),
+            ],
+          ),
+
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.access_alarm),
+              onPressed: () => {
+                    print('hello'),
+                  }),
+        ));
   }
 }
