2f0a6826913bdd35a66395347d0a733f9d76ee69
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-03-14 15:46:20 +0900

    chap02_flutter_onboarding

--- chap02_flutter_onboarding/lib/main.dart
+++ chap02_flutter_onboarding/lib/main.dart
@@ -1,19 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:intro_screen_onboarding_flutter/intro_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // backgroundColor: Color.fromARGB(255, 36, 34, 34),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TestScreen(),
+    );
+  }
+}
+
+class TestScreen extends StatelessWidget {
+  final List<Introduction> list = [
+    Introduction(
+      title: '현재의 나',
+      subTitle: '비어있는 박스뿐이지만',
+      imageUrl: 'assets/images/빈상자.jpg',
+    ),
+    Introduction(
+      title: '수료 후의 나',
+      subTitle: '좋은 내용물이 담길 수 있게 포장지가 깔려서',
+      imageUrl: 'assets/images/틀박스.jpg',
+    ),
+    Introduction(
+      title: '10년 후의 나',
+      subTitle: '좋은 내용물들이담겨있는보물상자가되어있을것이다',
+      imageUrl: 'assets/images/보물상자.jpg',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroScreenOnboarding(
+      introductionList: list,
+      onTapSkipButton: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ), //MaterialPageRoute
+        );
+      },
+      // foregroundColor: Colors.red,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Home Page!',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
